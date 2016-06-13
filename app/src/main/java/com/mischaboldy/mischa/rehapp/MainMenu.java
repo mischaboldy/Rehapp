c8dcb94d0a7f05adb346c490d316589a30990196
Mischa Boldy <mischa.boldy@gmail.com>
2016-06-13 13:12:17 +0200

    initial commit

---
+++ app/src/main/java/com/mischaboldy/mischa/rehapp/MainMenu.java
@@ -0,0 +1,13 @@
+package com.mischaboldy.mischa.rehapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
