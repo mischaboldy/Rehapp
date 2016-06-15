ab74cfca975c8a45068d6c3ddf1a434bbafe8d7a
Mischa Boldy <mischa.boldy@gmail.com>
2016-06-15 13:30:27 +0200

    adding toolbar with styling

--- app/src/main/java/com/mischaboldy/mischa/rehapp/MainMenu.java
+++ app/src/main/java/com/mischaboldy/mischa/rehapp/MainMenu.java
@@ -1,8 +1,12 @@
 package com.mischaboldy.mischa.rehapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
@@ -11,7 +15,30 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        TextView appTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+        appTitle.setText("Rehapp");
+
+        Typeface titleTypeFace = Typeface.createFromAsset(getAssets(), "fonts/KeepCalm-Medium.ttf");
+        appTitle.setTypeface(titleTypeFace);
     }
+
+    // Menu icons are inflated just as they were with actionbar
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 
     public void open_my_profile(View view) {
         Toast.makeText(this, "open my profile",
