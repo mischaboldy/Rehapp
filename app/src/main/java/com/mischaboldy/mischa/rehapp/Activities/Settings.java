517b4baaf3fa739b4d1c78d64a79d9518783c6da
Mischa Boldy <mischa.boldy@gmail.com>
2016-07-08 15:26:45 +0200

    updating settings

--- app/src/main/java/com/mischaboldy/mischa/rehapp/Activities/Settings.java
+++ app/src/main/java/com/mischaboldy/mischa/rehapp/Activities/Settings.java
@@ -1,13 +1,15 @@
 package com.mischaboldy.mischa.rehapp.Activities;
 
-import android.content.SharedPreferences;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.mischaboldy.mischa.rehapp.AchievementTracker;
 import com.mischaboldy.mischa.rehapp.R;
@@ -15,34 +17,81 @@ import com.mischaboldy.mischa.rehapp.R;
 /**
  * Created by mischa on 05/07/16.
  */
-public class Settings extends AppCompatActivity{
+public class Settings extends PreferenceActivity{
 
-    public static final String PREFS_NAME = "SettingsFile";
-    SharedPreferences settings;
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-
-        settings = getSharedPreferences(PREFS_NAME, 0);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-        TextView appTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
-        appTitle.setText(R.string.settings_title);
-
-//        ListAdapter listAdapter = new MedalsListViewAdapter(this, medalsList);
-//
-//        ListView listView = (ListView) findViewById(R.id.medals_list_view);
-//
-//        listView.setAdapter(listAdapter);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
     }
 
-    public void removeMedals(View view){
-        AchievementTracker.removeMedals(this);
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
+        root.addView(bar, 0); // insert at top
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+            Preference removeMedalsButton = findPreference("removeMedals");
+            removeMedalsButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Toast.makeText(getActivity(),"Medailles verwijderd",Toast.LENGTH_SHORT).show();
+                    removeMedals(getActivity());
+                    return true;
+                }
+            });
+
+            Preference removeWorkoutsButton = findPreference("removeWorkouts");
+            removeWorkoutsButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Toast.makeText(getActivity(),"Workouts verwijderd",Toast.LENGTH_SHORT).show();
+                    removeWorkouts();
+                    return true;
+                }
+            });
+
+            Preference removeProfileButton = findPreference("removeProfile");
+            removeProfileButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Toast.makeText(getActivity(),"Profiel verwijderd",Toast.LENGTH_SHORT).show();
+
+                    removeProfile();
+                    return true;
+                }
+            });
+        }
+
+    }
+
+    public static void removeMedals(Context context){
+        AchievementTracker.removeMedals(context);
+    }
+
+
+    public static void removeWorkouts(){
+    }
+
+
+    public static void removeProfile(){
     }
 }
